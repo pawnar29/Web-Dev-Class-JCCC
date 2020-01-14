131737bb80908bcec6148c5f847cde447c06a932
Narayana <pawnar29@gmail.com>
2020-01-14 00:53:39 -0600

    Add files via upload

---
+++ final/placeholder.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<!--Narayana Venkobasah Due May 12, 2016 Feedback-->
+<html lang="en">
+	<head>
+		<title>Feedback</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="icon" href="images/favicon.jpeg" type="image/x-icon">
+		<link href="style.css" rel="stylesheet" media="screen">
+		<link href="print.css" rel="stylesheet" media="print">
+	</head>
+	<body>
+		<div id="wrapper">
+			<header>
+				<h1>The Top 20 "Best Practices" for HTML and CSS</h1>
+			</header>
+			<nav>
+				<ul>
+					<li><a href="final1.html">Home</a></li>
+					<li><a href="final2.html">Practices of HTML</a></li>
+					<li><a href="final3.html">Practices of CSS</a></li>
+					<li><a href="final4.html">Credits</a></li>
+					<li><a href="final5.html">Feedback</a></li>
+				</ul>
+			</nav>
+			<main>
+				<div id="text-wrapper">
+					<h2>Thank you for your input.</h2>
+					<br>
+					<br>
+					<br>
+					<br>
+					<br>
+					<form action="#">
+						<input type="button" value="Back" onclick="javascript:history.go(-1)">
+					</form>
+					<p>&nbsp;</p>
+				</div>
+			</main>
+			<footer>
+				<ul>
+					<li><a href="final1.html">Home</a></li>
+					<li><a href="final2.html">Practices of HTML</a></li>
+					<li><a href="final3.html">Practices of CSS</a></li>
+					<li><a href="final4.html">Credits</a></li>
+					<li><a href="final5.html">Feedback</a></li>
+				</ul>
+				Copyright &copy; 2016<br>
+				<a href="mailto:npawarve@stumail.jccc.edu">npawarve@stumail.jccc.edu</a>
+			</footer>
+		</div>
+	</body>
+</html>
